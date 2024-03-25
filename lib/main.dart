9e1ee97fefdfcabf7babb1f0a27597aae74e3166
Ivan <77916322+kasyanovmafioznik@users.noreply.github.com>
2024-03-26 00:33:47 +0300

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:test_project_for_work/Home_Page.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Test',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
